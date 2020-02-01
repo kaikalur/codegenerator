9fc2e2173d2ce0ab5aa6eba628c451fd9dc19215
Markus Brigl <markus.brigl@tol.info>
2020-02-01 14:12:19 +0100

    Comment unused parameters

--- cpp/src/main/java/org/javacc/cpp/TokenManagerCodeGenerator.java
+++ cpp/src/main/java/org/javacc/cpp/TokenManagerCodeGenerator.java
@@ -380,7 +380,7 @@ class TokenManagerCodeGenerator implements org.javacc.parser.TokenManagerCodeGen
     codeGenerator.println("}");
 
     codeGenerator
-        .println("void " + tokenizerData.parserName + "TokenManager::SkipLexicalActions(Token * matchedToken) {");
+    .println("void " + tokenizerData.parserName + "TokenManager::SkipLexicalActions(Token * /*matchedToken*/) {");
     dumpLexicalActions(allMatches, TokenizerData.MatchType.SKIP, "jjmatchedKind", codeGenerator);
     dumpLexicalActions(allMatches, TokenizerData.MatchType.SPECIAL_TOKEN, "jjmatchedKind", codeGenerator);
     codeGenerator.println("}");
