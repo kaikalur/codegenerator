f049a3eee121124a0f8882da24ee3dff52440930
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-03-28 11:29:18 +0100

    Add MatcherTokenManager as UserTokenManager

--- cpp/examples/UserTokenManager/src/main/cpp/MatcherTokenManager.h
+++ cpp/examples/UserTokenManager/src/main/cpp/MatcherTokenManager.h
@@ -1,10 +1,24 @@
 #include "TokenManager.h"
+#include "MatcherConstants.h"
+using namespace Basic;
 
 class MatcherTokenManager : public Basic::TokenManager {
 public:
-	MatcherTokenManager(Basic::CharStream* cs) {
+	MatcherTokenManager(Basic::CharStream* cs) : cs(cs) {
 	}
 	virtual Basic::Token* getNextToken() {
-		return nullptr;
+		Basic::Token* token = nullptr;
+		JJChar jjchar = cs->readChar();
+		switch (jjchar) {
+		case '{': token = new Basic::Token(Basic::LBRACE, cs->getImage());  break;
+		case '}': token = new Basic::Token(Basic::RBRACE, cs->getImage());  break;
+		case '\n': token = new Basic::Token(Basic::NL, cs->getImage());  break;
+		case '\r': token = new Basic::Token(Basic::LF, cs->getImage());  break;
+		case 0: token = new Basic::Token(Basic::_EOF, cs->getImage());  break;
+
+		}
+		return token;
 	}
+private:
+	Basic::CharStream* cs;
 };
