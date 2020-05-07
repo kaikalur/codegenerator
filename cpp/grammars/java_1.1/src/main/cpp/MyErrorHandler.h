5582a6792cdbdb751346ee96deaa8862d1b20911
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-05-07 21:09:52 +0200

    Fix parser class

--- cpp/grammars/java_1.1/src/main/cpp/MyErrorHandler.h
+++ cpp/grammars/java_1.1/src/main/cpp/MyErrorHandler.h
@@ -9,9 +9,9 @@ class MyErrorHandler: public ErrorHandler {
 public:
 	MyErrorHandler();
 	virtual ~MyErrorHandler();
-	virtual void handleUnexpectedToken(int expectedKind, const JJString& expectedImage, const JJString& expectedLabel, const Token* actual, Parser* parser){}
-	virtual void handleParseError(const Token* last, const Token* unexpected, const JJSimpleString& production, Parser* parser){}
-	virtual void handleOtherError(const JJString& message, Parser* parser){}
+	virtual void handleUnexpectedToken(int expectedKind, const JJString& expectedImage, const JJString& expectedLabel, const Token* actual, JavaParser* parser){}
+	virtual void handleParseError(const Token* last, const Token* unexpected, const JJSimpleString& production, JavaParser* parser){}
+	virtual void handleOtherError(const JJString& message, JavaParser* parser){}
 
 };
 
