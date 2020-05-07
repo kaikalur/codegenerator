62c6375f7658a55445bb76512c58890959e68cea
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-05-07 21:15:26 +0200

    Fix missing cons/des/tructors

--- cpp/grammars/java_1.1/src/main/cpp/MyErrorHandler.h
+++ cpp/grammars/java_1.1/src/main/cpp/MyErrorHandler.h
@@ -7,8 +7,8 @@ namespace java { namespace parser {
 
 class MyErrorHandler: public ErrorHandler {
 public:
-	MyErrorHandler();
-	virtual ~MyErrorHandler();
+	MyErrorHandler() {}
+	virtual ~MyErrorHandler() {}
 	virtual void handleUnexpectedToken(int expectedKind, const JJString& expectedImage, const JJString& expectedLabel, const Token* actual, JavaParser* parser){}
 	virtual void handleParseError(const Token* last, const Token* unexpected, const JJSimpleString& production, JavaParser* parser){}
 	virtual void handleOtherError(const JJString& message, JavaParser* parser){}
