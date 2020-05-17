c73504a77976b31eb29777814bf1a027eb857d1a
Francis ANDRE <francis.andre.kampbell@orange.fr>
2020-05-17 17:10:57 +0200

    vs2019 solution

--- cpp/bugs/bug001/src/main/cpp/Main.cpp
+++ cpp/bugs/bug001/src/main/cpp/Main.cpp
@@ -25,8 +25,39 @@ using namespace ASN1::Typer;
 JJString ReadFileFully() {
 	JJString code;
 	code = 
-		"-- OK: Everything is fine\n"
-		"ModuleTestEmpty-00 { iso org(3) } DEFINITIONS ::= BEGIN END\n";	
+"\r\n"
+"-- OK: Everything is fine\r\n"
+"\r\n"
+"-- iso.org.dod.internet.private.enterprise (1.3.6.1.4.1)\r\n"
+"-- .spelio.software.asn1c.test (9363.1.5.1)\r\n"
+"-- .10\r\n"
+"\r\n"
+"ModuleTestInt-10\r\n"
+"	{ iso org(3) dod(6) internet (1) private(4) enterprise(1)\r\n"
+"		spelio(9363) software(1) asn1c(5) test(1) 10 }\r\n"
+"	DEFINITIONS ::=\r\n"
+"BEGIN\r\n"
+"\r\n"
+"	alpha INTEGER ::= 1\r\n"
+/*
+"	Type1 ::= INTEGER { alpha(2) }\r\n"
+"	Type2 ::= INTEGER { alpha(3), beta(alpha) }\r\n"
+"	gamma Type2 ::= beta	-- equals 1 --\r\n"
+"	delta Type2 ::= alpha	-- equals 3 --\r\n"
+"\r\n"
+
+"	/*\r\n"
+"	 *  The following are for post-fix checking by the check_fixer.\r\n"
+"	 * It will be able to pick-up these values if the file is parseable,\r\n"
+"	 * even if it contains some semantic errors.\r\n"
+"	 \r\n"
+"\r\n"
+"	check-gamma INTEGER ::= 1	-- check value\r\n"
+"	check-delta INTEGER ::= 3	-- check value\r\n"
+"\r\n"
+*/
+"END\r\n"
+;
 	return code;
 }
 static void usage(int argc, char**argv) {
@@ -47,7 +78,7 @@ int main(int argc, char**argv) {
 			switch(argc) {
 				case 4: efs.open(argv[3]);
 				case 3: ofs.open(argv[2]);
-				case 2: ifs.open(argv[1]);
+				case 2: ifs.open(argv[1], ifstream::binary);
 			}
 			if (ifs.is_open()) {
 				input = &ifs;
@@ -76,6 +107,7 @@ int main(int argc, char**argv) {
 		}
 		TokenManager *scanner = new TyperTokenManager(cs);
 		Typer parser(scanner);
+
 		parser.ModuleDefinitionList();
      	*output << "Parser Version 0.1:  file parsed successfully." << endl;
 	} catch (const ParseException& e) {
